4a97591ee12a8a558c2c06eff18a0b36dd145fc7
AustinLeeGordon <gordon.austin2@gmail.com>
2020-05-15 10:46:56 -0500

    Added error handling

--- index.js
+++ index.js
@@ -1,103 +1,121 @@
-const NOT_ANSWERED_REGEX = /^(not\sanswered)$/i
+const NOT_ANSWERED_REGEX = /^(not\sanswered)$/i;
 
 const parseRating = (val, options = {}) => {
-    const { min = 0, max = 10 } = options
-    const numberVal = parseInt(val)
+    const { min = 0, max = 10 } = options;
+    const numberVal = parseInt(val);
 
     if (numberVal >= min && numberVal <= max) {
-        return numberVal
+        return numberVal;
     }
 
-    return ''
-}
+    return '';
+};
 
-const trim = str => {
-    return str.trim()
-}
+const trim = (str) => {
+    return str.trim();
+};
 
-const isDefined = val => {
-    return typeof val !== 'undefined' && val !== null
-}
+const isDefined = (val) => {
+    return typeof val !== 'undefined' && val !== null;
+};
 
-const formatStringResponse = val => {
-    const formattedVal = trim(val)
-    if (!isDefined(formattedVal) || NOT_ANSWERED_REGEX.test(formattedVal)) {
-        return ''
+const formatStringResponse = (val) => {
+    if (!isDefined(formattedVal)) {
+        return '';
     }
 
-    return formattedVal
-}
+    const formattedVal = trim(val);
+    if (NOT_ANSWERED_REGEX.test(formattedVal)) {
+        return '';
+    }
 
-const getValueOrFallback = val => {
+    return formattedVal;
+};
+
+const getValueOrFallback = (val) => {
+    if (!isDefined(formattedVal)) {
+        return '';
+    }
+
     const arr = val
         .split('|')
         .map(trim)
-        .filter(str => !NOT_ANSWERED_REGEX.test(str))
+        .filter((str) => !NOT_ANSWERED_REGEX.test(str));
 
     if (arr[0]) {
-        return arr[0]
+        return arr[0];
     }
 
-    return ''
-}
+    return '';
+};
 
 module.exports.format = (event, context, callback) => {
-    const data = JSON.parse(event.body)
+    try {
+        const data = JSON.parse(event.body);
 
-    const recommendHutson = parseRating(data.recommendHutson)
+        const recommendHutson = parseRating(data.recommendHutson);
 
-    const onlineRating = parseRating(data.onlineRating)
+        const onlineRating = parseRating(data.onlineRating);
 
-    const partsRating = parseRating(data.partsRating)
+        const partsRating = parseRating(data.partsRating);
 
-    const serviceRating = parseRating(data.serviceRating)
+        const serviceRating = parseRating(data.serviceRating);
 
-    const salesRating = parseRating(data.salesRating)
+        const salesRating = parseRating(data.salesRating);
 
-    const offeredPartsAlternatives = formatStringResponse(data.offeredPartsAlternatives)
+        const offeredPartsAlternatives = formatStringResponse(
+            data.offeredPartsAlternatives
+        );
 
-    const accurateQuote = formatStringResponse(data.accurateQuote)
+        const accurateQuote = formatStringResponse(data.accurateQuote);
 
-    const contactedAfterPurchase = formatStringResponse(data.contactedAfterPurchase)
+        const contactedAfterPurchase = formatStringResponse(
+            data.contactedAfterPurchase
+        );
 
-    const onlineSuggestions = formatStringResponse(data.onlineSuggestions)
+        const onlineSuggestions = formatStringResponse(data.onlineSuggestions);
 
-    const comments = formatStringResponse(data.comments)
+        const comments = formatStringResponse(data.comments);
 
-    const changesForHigherRating = formatStringResponse(data.changesForHigherRating)
+        const changesForHigherRating = formatStringResponse(
+            data.changesForHigherRating
+        );
 
-    const needToContact = getValueOrFallback(data.needToContact)
+        const needToContact = getValueOrFallback(data.needToContact);
 
-    const name = getValueOrFallback(data.name)
+        const name = getValueOrFallback(data.name);
 
-    const company = getValueOrFallback(data.company)
+        const company = getValueOrFallback(data.company);
 
-    const address = getValueOrFallback(data.address)
+        const address = getValueOrFallback(data.address);
 
-    const phone = getValueOrFallback(data.phone)
+        const phone = getValueOrFallback(data.phone);
 
-    const email = getValueOrFallback(data.email)
+        const email = getValueOrFallback(data.email);
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            recommendHutson,
-            onlineRating,
-            partsRating,
-            serviceRating,
-            salesRating,
-            offeredPartsAlternatives,
-            accurateQuote,
-            contactedAfterPurchase,
-            onlineSuggestions,
-            comments,
-            changesForHigherRating,
-            needToContact,
-            name,
-            company,
-            address,
-            phone,
-            email,
-        }),
-    })
-}
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                recommendHutson,
+                onlineRating,
+                partsRating,
+                serviceRating,
+                salesRating,
+                offeredPartsAlternatives,
+                accurateQuote,
+                contactedAfterPurchase,
+                onlineSuggestions,
+                comments,
+                changesForHigherRating,
+                needToContact,
+                name,
+                company,
+                address,
+                phone,
+                email,
+            }),
+        });
+    } catch (error) {
+        callback(Error(error));
+    }
+};
